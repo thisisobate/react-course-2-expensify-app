b72c4ab2b2082553b65e8fbcfd1df4c3d065fb4b
Uchechukwu Obasi <obasiuche62@gmail.com>
2021-01-15 12:03:23 +0100

    fixed add expense button

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { startAddExpense } from '../actions/expenses';
+import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.startAddExpense(expense);
+    this.props.addExpense(expense);
     this.props.history.push('/');
   };
   render() {
@@ -27,7 +27,7 @@ export class AddExpensePage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  startAddExpense: (expense) => dispatch(startAddExpense(expense))
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
